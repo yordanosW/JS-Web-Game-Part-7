eae5f4ea8bb13c6d8179c04185c64c6145d49aac
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-10-15 11:58:48 -0700

    edit

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -31,27 +31,26 @@ function newNonPlayableCharacter(x, y) {
          stop()
         
     }
-
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
-
-    function stop() {
-        direction = null
-        element.src = `./assets/red-character/static.gif`
-    }
-
     return {
         element: element,
         walkWest: walkWest,
